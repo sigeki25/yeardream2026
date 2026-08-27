62a2eb603fa2b081d0178bb3e1a688255e5da355
중진공06 <sigeki25@gmail.com>
2026-08-27 09:48:42 +0900

    rtl

--- next_js/01_jest/src/__tests__/calcModule.test.js
+++ next_js/01_jest/src/__tests__/calcModule.test.js
@@ -4,19 +4,30 @@
 
 import {divide, minus, multiply, plus} from "@/app/calcModule";
 
-describe("시착연산 테스트", () => {
-    test("더하기 모듈 테스트", () => {
-        expect(plus(10, 30)).toBe(40)
-    })
-    test("빼기 모듈 테스트", () => {
-        expect(minus(30, 10)).toBe(20)
-    })
-    test("곱하기 모듈 테스트", () => {
-        expect(multiply(10, 30)).toBe(300)
-    })
-    test("나누기 모듈 테스트", () => {
-        expect(divide(30, 10)).toBe(3)
-    })
+describe("시착연산 통합 테스트(정상, 에러)", () => {
+    describe("시착연산 테스트", () => {
+        test("더하기 모듈 테스트", () => {
+            expect(plus(10, 30)).toBe(40)
+        });
+        test("빼기 모듈 테스트", () => {
+            expect(minus(30, 10)).toBe(20)
+        });
+        test("곱하기 모듈 테스트", () => {
+            expect(multiply(10, 30)).toBe(300)
+        });
+        test("나누기 모듈 테스트", () => {
+            expect(divide(30, 10)).toBe(3)
+        });
+    });
+
+    describe("시착연산 에러 테스트", () => {
+        test("a 보다 b 값이 큻 경우", () => {
+            expect(() => minus(10, 30)).toThrow("뺄셈릐 값은 0보다 커야 합니다.");
+        });
+        test("0으로 나누기 시도", () => {
+            expect(() => divide(10, 0)).toThrow("0으로 나눌 수 없습니다.");
+        });
+    });
 });
 /*
 toBe() : 숫자, 문자, 불리언 타입의 값이 일치
--- next_js/01_jest/src/app/calcModule.jsx
+++ next_js/01_jest/src/app/calcModule.jsx
@@ -1,9 +1,13 @@
 export function plus(a, b) {return a + b}
-export function minus(a, b) {return a - b}
+export function minus(a, b) {
+    if (a - b < 0)
+        throw new Error("뺄셈릐 값은 0보다 커야 합니다.");
+    return a - b
+}
 export function multiply(a, b) {return a * b}
 export function divide(a, b) {
     if (b === 0)
-        throw new Error("0으로 나눌 수 없습니다.")
+        throw new Error("0으로 나눌 수 없습니다.");
     return a / b
 }
 
